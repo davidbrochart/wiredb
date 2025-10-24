b6dbe12d8e28eb48dbd976bd471143d49f619311
David Brochart <david.brochart@gmail.com>
2025-10-24 15:36:12 +0200

    Use lock when accessing file

--- wires/file/src/wire_file/client_wire.py
+++ wires/file/src/wire_file/client_wire.py
@@ -6,7 +6,7 @@ from contextlib import asynccontextmanager
 from pathlib import Path
 
 import anyio
-from anyio import AsyncContextManagerMixin, CancelScope, TASK_STATUS_IGNORED, create_memory_object_stream, create_task_group, open_file, sleep
+from anyio import AsyncContextManagerMixin, AsyncFile, CancelScope, Lock, TASK_STATUS_IGNORED, create_memory_object_stream, create_task_group, open_file, sleep
 from anyio.abc import TaskGroup, TaskStatus
 from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
 from pycrdt import Channel, Decoder, Doc, YMessageType, YSyncMessageType, create_sync_message, handle_sync_message, write_message
@@ -33,24 +33,20 @@ class ClientWire(AsyncContextManagerMixin, _ClientWire):
         self._id = id
         self._path: anyio.Path = anyio.Path(path)
         self._write_delay = write_delay
-        self._version = "0.0.1"
         self._squash = squash
+        self._version = "0.0.1"
+        self._lock = Lock()
 
     @property
     def version(self) -> str:
         return self._version
-
-    async def _read_file(self) -> tuple[str, bytes]:
-        data = await self._path.read_bytes()
-        version, messages = data.split(bytes([0]), 1)
-        return version.decode(), messages
 
     @asynccontextmanager
     async def __asynccontextmanager__(self) -> AsyncGenerator[Self]:
         file_doc: Doc = Doc()
         size = len(self._version) + 1
         if file_exists := await self._path.exists():
-            file_version, messages = await self._read_file()
+            file_version, messages = await read_file(self._path, self._lock)
             if file_version != self._version:
                 raise RuntimeError(f'File version mismatch (got "{file_version}", expected "{self._version}")')
             size += len(messages)
@@ -63,15 +59,15 @@ class ClientWire(AsyncContextManagerMixin, _ClientWire):
         async with file_doc.new_transaction():
             sync_message = create_sync_message(file_doc)
         async with await open_file(self._path, mode="a+b", buffering=0) as self._file:
-            if self._squash:
-                await squash(self._file)
             if not file_exists:
                 with CancelScope(shield=True):
-                    await self._file.write(self._version.encode() + bytes([0]))
+                    await write_file(self._file, self._version.encode() + bytes([0]), self._lock)
+            elif self._squash:
+                await squash_file(self._file, self._lock)
             send_stream, receive_stream = create_memory_object_stream[bytes](max_buffer_size=float("inf"))
             async with send_stream, receive_stream, create_task_group() as tg:
                 await send_stream.send(sync_message)
-                channel = File(self._file, self._id, file_doc, send_stream, receive_stream, tg, self._write_delay, size, self._squash, self._version)
+                channel = File(self._file, self._id, file_doc, send_stream, receive_stream, tg, self._write_delay, size, self._squash, self._version, self._lock)
                 async with Provider(self._doc, channel):
                     yield self
                     tg.cancel_scope.cancel()
@@ -80,7 +76,7 @@ class ClientWire(AsyncContextManagerMixin, _ClientWire):
 class File(Channel):
     def __init__(
         self,
-        file: anyio.AsyncFile[bytes],
+        file: AsyncFile[bytes],
         path: str,
         file_doc: Doc,
         send_stream: MemoryObjectSendStream[bytes],
@@ -90,17 +86,18 @@ class File(Channel):
         size: int,
         squash: bool,
         version: str,
+        lock: Lock,
     ) -> None:
         self._file = file
         self._path = path
         self._file_doc: Doc | None = file_doc
         self._send_stream = send_stream
         self._receive_stream = receive_stream
+        self._task_group = task_group
         self._write_delay = write_delay
-        self._size = size
         self._squash = squash
         self._version = version
-        self._task_group = task_group
+        self._lock = lock
         self._messages: list[bytes] = []
         self._write_cancel_scope: CancelScope | None = None
 
@@ -149,25 +146,40 @@ class File(Channel):
                 messages = b"".join(self._messages)
                 self._messages.clear()
                 self._write_cancel_scope = None
-                self._size += len(messages)
-                await self._file.write(messages)
                 if self._squash:
-                    await squash(self._file)
+                    await squash_file(self._file, self._lock, messages)
+                else:
+                    await write_file(self._file, messages, self._lock)
 
 
-async def squash(file: anyio.AsyncFile[bytes]) -> None:
-    await file.seek(0)
-    data = await file.read()
-    version, messages = data.split(bytes([0]), 1)
-    header_size = len(version) + 1
-    await file.truncate(header_size)
-    file_doc: Doc = Doc()
-    decoder = Decoder(messages)
-    while True:
-        update = decoder.read_message()
-        if not update:
-            break
-        file_doc.apply_update(update)
-    squashed_update = file_doc.get_update()
-    message = write_message(squashed_update)
-    await file.write(message)
+async def read_file(path: anyio.Path, lock: Lock) -> tuple[str, bytes]:
+    async with lock:
+        data = await path.read_bytes()
+        version, messages = data.split(bytes([0]), 1)
+        return version.decode(), messages
+
+
+async def write_file(file: AsyncFile[bytes], data: bytes, lock: Lock) -> None:
+    async with lock:
+        await file.write(data)
+
+
+async def squash_file(file: AsyncFile[bytes], lock: Lock, with_messages: bytes | None = None) -> None:
+    async with lock:
+        await file.seek(0)
+        data = await file.read()
+        version, messages = data.split(bytes([0]), 1)
+        header_size = len(version) + 1
+        await file.truncate(header_size)
+        file_doc: Doc = Doc()
+        if with_messages is not None:
+            messages += with_messages
+        decoder = Decoder(messages)
+        while True:
+            update = decoder.read_message()
+            if not update:
+                break
+            file_doc.apply_update(update)
+        squashed_update = file_doc.get_update()
+        message = write_message(squashed_update)
+        await file.write(message)
